9087fb098ec5819088619fb94763daac3a70b447
Mashcode <marc@marcshifflett.com>
2014-12-21 13:58:45 -0500

    programming assignment updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 ## Write a short comment describing this function
-## This creates a cachematrix object where x is defined as a matrix
+## This function ncreates a CacheMatrix object where x is defined as a matrix
 makeCacheMatrix <- function(x = matrix()) {
         ## NULL is assigned the variable m
         m <- NULL
-        ## Sets the value of the matrix
+        ## Set the value of the matrix
         set <- function(y) {
                 ## y is superassigned x which is defined as a matrix
                 x <<- y
@@ -16,26 +15,29 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         ## Sets the value and gets the value of the inverse
         setinverse <- function(solve) m <<- solve
+        ## The following is code I can't comment on intelligently
+        ## A list of elements of getinverse?
         getinverse <- function() m
                 list(set = set, get = get,
                      setinverse = setinverse,
                      getinverse = getinverse)
 }
 
-## cacheSolve function takes the arguments x, in this case a matrix,and ... 
+## The cacheSolve function takes the arguments x, in this case a matrix,and ... 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## ...returns a matrix that is the inverse of 'x'
         ## gets the inverse of the matrix and assigns it to m
         m <- x$getinverse()
         
-        ## if m is NOT a null value, previously solved, then print message
+        ## if solution is previously cached print message
+        ## else return null
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         ## otherwise assign the get function a name 'data'
         data <- x$get()
-        ## and passes in 'data' as an argument in the solve function assigned to m
+        ## and pass in 'data' as an argument in the solve function assigned to m
         m <- solve(data, ...)
         ## ...which sets the inverse of the cached matrix 
         x$setinverse(m)
